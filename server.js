83a0b92d746cabcde619e26cdf73ccc8e3b26c03
filipesk004-blip <filipesk004@gmail.com>
2026-09-17 17:50:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,67 +2,68 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const path = require('path');
 
-// Servíruje statické soubory ze složky 'public'
 app.use(express.static('public'));
 
-// Načte index.html ze složky 'public' při přístupu na hlavní stránku
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 let players = {};
+let hunterId = null;
+
+const PROP_TYPES = ['box', 'barrel', 'rock', 'table', 'pumpkin'];
 
 io.on('connection', (socket) => {
+    console.log('Hráč připojen:', socket.id);
+
+    // První připojený je Hunter, ostatní jsou Čarodějnice
+    if (!hunterId) {
+        hunterId = socket.id;
+    }
+
+    const isHunter = (socket.id === hunterId);
     players[socket.id] = {
-        x: (Math.random() - 0.5) * 60,
-        z: (Math.random() - 0.5) * 60,
+        id: socket.id,
+        x: (Math.random() - 0.5) * 20,
+        y: 1.6,
+        z: (Math.random() - 0.5) * 20,
         rotation: 0,
-        kills: 0,
-        isCrouching: false,
-        name: 'Hráč'
+        role: isHunter ? 'hunter' : 'witch',
+        propType: isHunter ? 'human' : 'barrel',
+        hp: isHunter ? 100 : 2
     };
 
-    socket.emit('currentPlayers', players);
-    socket.broadcast.emit('newPlayer', { id: socket.id, player: players[socket.id] });
+    socket.emit('initGame', { id: socket.id, players: players, propTypes: PROP_TYPES });
+    socket.broadcast.emit('newPlayer', players[socket.id]);
 
     socket.on('playerMove', (data) => {
         if (players[socket.id]) {
             players[socket.id].x = data.x;
+            players[socket.id].y = data.y;
             players[socket.id].z = data.z;
             players[socket.id].rotation = data.rotation;
-            players[socket.id].isCrouching = data.isCrouching;
-            if (data.name) players[socket.id].name = data.name;
-            socket.broadcast.emit('playerMoved', { id: socket.id, player: players[socket.id] });
+            socket.broadcast.emit('playerMoved', players[socket.id]);
         }
     });
 
+    // Čarodějnice mění svou podobu
+    socket.on('changeProp', (newProp) => {
+        if (players[socket.id] && players[socket.id].role === 'witch') {
+            players[socket.id].propType = newProp;
+            io.emit('propChanged', { id: socket.id, propType: newProp });
+        }
+    });
+
+    // Hunter střílí
     socket.on('shoot', (data) => {
-        const targetId = data.hitPlayerId;
-        if (players[targetId]) {
-            io.to(targetId).emit('takeDamage', { damage: data.damage, attackerId: socket.id });
-            socket.emit('hitConfirmed');
-        }
-    });
-
-    socket.on('throwFlashbang', (data) => socket.broadcast.emit('spawnFlashbang', data));
-    socket.on('flashbangExploded', (data) => io.emit('flashbangDetonated', data));
-
-    socket.on('respawnRequest', () => {
-        if (players[socket.id]) {
-            players[socket.id].x = (Math.random() - 0.5) * 60;
-            players[socket.id].z = (Math.random() - 0.5) * 60;
-        }
+        socket.broadcast.emit('hunterShot', data);
     });
 
     socket.on('disconnect', () => {
         delete players[socket.id];
+        if (hunterId === socket.id) {
+            hunterId = Object.keys(players)[0] || null;
+            if (hunterId) players[hunterId].role = 'hunter';
+        }
         io.emit('playerDisconnected', socket.id);
     });
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-    console.log(`Server běží na portu ${PORT}`);
-});
+http.listen(3000, () => console.log('Server běží na http://localhost:3000'));
