f0a871a89b0878c289b5449502361004e6682e8e
filipesk004-blip <filipesk004@gmail.com>
2026-09-17 17:58:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -66,4 +66,5 @@ io.on('connection', (socket) => {
     });
 });
 
-http.listen(3000, () => console.log('Server běží na http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => console.log(`Server běží na portu ${PORT}`));
